25ecf414f63dafca03c6749dda32f375c6da0f80
Chris Donnelly <cdonnelly@69bytes.com>
2024-07-08 17:44:57 -0500

    Basic install 2/2

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,37 +1,68 @@
-# Site settings
-title: Finally, a blog!
-description: Because the Internet doesn't have enough blogs already. #
-url: http://www.chrisdonnelly.com
-baseurl: ""
-author: Chris R. Donnelly
-email: blog@chrisdonnelly.com
-lang: en-US
-repository: cdonnellytx/cdonnellytx.github.io
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight')
+const includeDrafts = false
 
-# UX
-remote_theme: StartBootstrap/startbootstrap-clean-blog-jekyll
-image: "/assets/img/bg/home.jpg"
-permalink: /:categories/:year/:month/:day/:title
-feed_url: "/feed.xml"
+/**
+ * @param {import('@11ty/eleventy').UserConfig} eleventyConfig
+ * @returns {{ dir: Record<string, string?>}}
+ */
+module.exports = (eleventyConfig) => {
+    eleventyConfig.addPlugin(syntaxHighlight)
 
-# Social
-# twitter_username: cdonnellytx
-github_username: cdonnellytx
-# facebook_username: cdonnellytx
-stackoverflow_link: users/17152/chris-r-donnelly
-mastodon_link: hachyderm.io/@cdonnellytx
+    /**
+     * @param {import('@11ty/eleventy').collectionApi} collectionApi
+     */
+    eleventyConfig.addCollection('posts', collectionApi => {
+        return collectionApi.getFilteredByGlob([
+            "blog/_posts/*",
+            ...(includeDrafts ? ["blog/_drafts/*"] : []),
+        ]).filter(p => p.data.published !== false).reverse()
+    })
 
-# Build settings
-include:
-  - .well-known
+    eleventyConfig.addPassthroughCopy('.well-known')
+    eleventyConfig.addPassthroughCopy('zalgo')
 
-exclude:
-  - .editorconfig
-  - Gemfile
-  - Gemfile.lock
-  - LICENSE
-  - README.md
-  - .vscode
+    const unhandled = {
+        // Site settings
+        "title": "Finally, a blog!",
+        "description": "Because the Internet doesn't have enough blogs already.",
+        "url": "http://www.chrisdonnelly.com",
+        "baseurl": "",
+        "author": "Chris R. Donnelly",
+        "email": "blog@chrisdonnelly.com",
+        "lang": "en-US",
+        "repository": "cdonnellytx/cdonnellytx.github.io",
 
-plugins:
-  - jekyll-feed
+        // UX
+        "remote_theme": "StartBootstrap/startbootstrap-clean-blog-jekyll",
+        "image": "/assets/img/bg/home.jpg",
+        "permalink": "/:categories/:year/:month/:day/:title",
+        "feed_url": "/feed.xml",
+
+        // Social
+        // "twitter_username": "cdonnellytx",
+        "github_username": "cdonnellytx",
+        // "facebook_username": "cdonnellytx",
+        "stackoverflow_link": "users/17152/chris-r-donnelly",
+        "mastodon_link": "hachyderm.io/@cdonnellytx",
+
+        // Build settings
+        "exclude": [
+            ".editorconfig",
+            "Gemfile",
+            "Gemfile.lock",
+            "LICENSE",
+            "README.md",
+            ".vscode"
+        ]
+    }
+
+    return {
+        dir: {
+            input: "./",
+            output: "./_site",
+            layouts: "./_layouts"
+        }
+    }
+}
+
+
